f46aaea916243a5b9131d79c49db25e29e57a048
Austin Hill <aghman@gmail.com>
2021-02-22 08:57:18 -0800

    Adding python code to show preview window from camera

--- capture.py
+++ capture.py
@@ -1,9 +1,22 @@
 import serial
 import time
+import os
+from picamera import PiCamera
 
 ser=serial.Serial("/dev/ttyACM0",9600)  #change ACM number as found from ls /dev/tty/ACM*
 ser.baudrate=9600
+camera = PiCamera()
+
+
 
 while True:
   read_ser=ser.readline()
   print(read_ser)
+  camera.start_preview()
+  sleep(20)
+  camera.stop_preview()
+  
+
+  
+
+
