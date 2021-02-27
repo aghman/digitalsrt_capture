9618c009669e98546a4615febe9576d20ef50750
Austin Hill <aghman@gmail.com>
2021-02-26 22:15:18 -0800

    Breaking into functions, adding comments, using DCIM directory

--- capture.py
+++ capture.py
@@ -3,20 +3,48 @@ import time
 import os
 from picamera import PiCamera
 
-ser=serial.Serial("/dev/ttyACM0",9600)  #change ACM number as found from ls /dev/tty/ACM*
-ser.baudrate=9600
-camera = PiCamera()
+saveLocation = "/mnt/photoshare/DCIM"
+
+def prepDirectories():
+    print("Preparing DCIM directory...")
+    dirExists = os.path.exists(dirExists)
+    if !dirExists:
+        # Make directory
+        print("Creating DCIM directory...")
+        os.mkdir(saveLocation)
+
+    isADir = os.path.isdir(saveLocation)
+    dirExists = os.path.exists(dirExists)
+    if !dirExists || !isADir:
+        #bail
+        print("Failed to create DCIM directory. Exiting.")
+        exit(1)
+
+def connectSerial():
+    serialPort=serial.Serial("/dev/ttyACM0",9600)  #change ACM number as found from ls /dev/tty/ACM*
+    serialPort.baudrate=9600
+
+    ## Error handling???
+    return serialPort
 
 
-
-while True:
-  read_ser=ser.readline()
-  print(read_ser)
-  camera.start_preview()
-  time.sleep(20)
-  camera.stop_preview()
   
 
-  
+def main():
+    prepDirectories()
+    serialPort = connectSerial()
+    camera = PiCamera()
+    
+    camera.start_preview()
+    while True:
+        serialOutput=serialPort.readline()
+        print(serialOutput)
+        camera.capture()
+    camera.stop_preview()
+
+
+if __name__ == "__main__":
+    main()
+
 
 
